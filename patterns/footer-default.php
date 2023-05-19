583c136ef5a6aec1201155a50695dc6e03ee0957
Eric Phillips <github@nekvtcode.com>
2023-05-19 03:46:44 -0400

    Add default footer template

---
+++ patterns/footer-default.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Simple Bridge Theme Footer
+ * Slug: bridge/footer-default
+ * Categories: footer
+ * Block Types: core/navigation
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide"><!-- wp:site-logo {"shouldSyncIcon":true} /-->
+
+<!-- wp:navigation {"ref":1832} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
