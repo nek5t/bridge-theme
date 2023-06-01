443f75d67ab2176288b2ce81c8e60644546f3891
Eric Phillips <github@nekvtcode.com>
2023-06-01 08:18:07 -0400

    Update block style registration
    Move logic to separate function
    
    Change file naming so that blocks from any vendor can be targeted

--- functions.php
+++ functions.php
@@ -14,21 +14,30 @@ define( 'BRIDGE_THEME_VER', wp_get_theme()->get( 'Version' ) );
 
 function bridge_theme_setup() {
 	add_theme_support( 'wp-block-styles' );
-	$block_styles_dir = get_template_directory() . '/assets/css/dist/blocks';
+	bridge_enqueue_block_styles();
+}
+add_action( 'after_setup_theme', 'bridge_theme_setup' );
 
-	$core_block_styles = glob( $block_styles_dir . '/core-*.css', GLOB_NOSORT );
+function bridge_enqueue_block_styles() {
+	$block_styles_dir = 'assets/css/dist/blocks';
 
-	foreach( $core_block_styles as $block_style ) {
-		$block_name = str_replace( array( $block_styles_dir, '/core-', '.min', '.css' ), '',  $block_style );
+	$block_styles = glob( trailingslashit( get_template_directory() ) . $block_styles_dir . '/*.css', GLOB_NOSORT );
+
+	foreach( $block_styles as $block_style ) {
+		$style_path = ltrim( str_replace( get_template_directory(), '', $block_style ), '/' );
+
+		$block = explode( '.', str_replace( array( $block_styles_dir, '/', '.min', '.css' ), '', $style_path ), 2 );
+
+		$block_vendor = $block[0];
+		$block_name = $block[1];
 
 		$args = array(
 			'handle' => "bridge-$block_name",
-			'src' => get_template_directory_uri( $block_style ),
+			'src' => get_template_directory_uri( $style_path ),
 			'path' => $block_style,
 			'ver' => BRIDGE_THEME_VER
 		);
 
-		wp_enqueue_block_style( "core/$block_name", $args );
+		wp_enqueue_block_style( "$block_vendor/$block_name", $args );
 	}
 }
-add_action( 'after_setup_theme', 'bridge_theme_setup' );
