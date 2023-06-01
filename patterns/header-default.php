7bf884d39623f3969c50266cfbd23e550eb905cd
Eric Phillips <github@nekvtcode.com>
2023-06-01 07:20:28 -0400

    Add simple header template

---
+++ patterns/header-default.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Simple Bridge Theme Header
+ * Slug: bridge/header-default
+ * Categories: header
+ * Block Types: core/navigation
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group alignwide"><!-- wp:site-logo /-->
+
+<!-- wp:navigation {"ref":1832} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
